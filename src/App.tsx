2dff9cd3b94a7d950e91ce5a4bcd95b59f3ed1f2
Yuli <yu.expert@gmail.com>
2025-11-04 16:14:32 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        
+        Click on the Vite and React logos to learn more
+      </div>
+    </>
+  )
+}
+
+export default App
